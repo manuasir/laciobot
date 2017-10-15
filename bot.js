efe460845e1557f4681d3a47d37614e2090f3a59
manuasir <manu.asi2@gmail.com>
2017-10-16 01:05:23 +0200

    sending to production with webhooks :+1:

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ const http          = require('http');
 const _          = require('lodash');
 
 // MongoDB connection
-mongoose.connect('mongodb://manuasir:Mlab51617-@ds147964.mlab.com:47964/laciobot',{useMongoClient:true});
+const dbUrl = process.env.dburl;
+mongoose.connect(dbUrl,{useMongoClient:true});
 
 // mongoose.connect('mongodb://localhost/dev-laciobot');
 mongoose.Promise = global.Promise;
@@ -13,15 +14,26 @@ mongoose.connection.on('error',function(err){
   console.error("el err",err)
 });
 
-//This is because of Heroku issues
+//This is because of Heroku can listen as webhook
 http.createServer(function (request, response) {}).listen(process.env.PORT || 8081);
 
 const token = process.env.TOKEN;
 
-const bot = new TelegramBot({
+const conf = process.env.NODE === "production" ? {
+  token:token,
+  webhook: { // Optional. Use webhook instead of polling.
+    url: 'https://laciobot.herokuapp.com', // HTTPS url to send updates to.
+    host: '0.0.0.0', // Webhook server host.
+    port: 443, // Server port.
+    maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+  },
+  usePlugins: ['askUser','commandButton']
+} : {
   token:token,
   usePlugins: ['askUser','commandButton']
-});
+}
+
+const bot = new TelegramBot(conf);
 
 const init = require('./lib/index')(bot);
 
