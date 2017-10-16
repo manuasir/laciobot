6d3c768d950a36410c6e82aad75bf07082317c5f
manuasir <manu.asi2@gmail.com>
2017-10-16 03:17:42 +0200

    adding webhook

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const packageInfo = require('./package.json')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+/**
+ * Devolver version
+ */
+app.get('/', (req, res) => {
+  res.json({ version: packageInfo.version })
+})
+
+const server = app.listen(process.env.PORT, '0.0.0.0', () => {
+  const host = server.address().address
+  const port = server.address().port
+  console.log('Web server started at http://%s:%s', host, port)
+})
+
+module.exports = (bot) => {
+  app.post('/' + bot.token, (req, res) => {
+    bot.getBot().getWebhookInfo()
+    res.sendStatus(200)
+  })
+}
