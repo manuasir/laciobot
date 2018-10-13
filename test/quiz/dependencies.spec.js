5c63802c38cf346363ed9845869ed38d4e17f419
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-10-13 11:03:28 +0200

    Changes to match repos codestyle

--- test/quiz/dependencies.spec.js
+++ test/quiz/dependencies.spec.js
@@ -3,26 +3,26 @@
   For modules without export (like CLIs) add them to the dropModules array
 */
 
-import test from 'ava'
-import { dependencies, devDependencies } from '../../package.json'
+import test from "ava";
+import { dependencies, devDependencies } from "../../package.json";
 
-const dropModules = []
-const isDropped = module => !dropModules.includes(module)
+const dropModules = [];
+const isDropped = (module) => !dropModules.includes(module);
 
-test('basic check', t => {
-  t.true(true, 'ava works ok')
-})
+test("basic check", (t) => {
+  t.true(true, "ava works ok");
+});
   
-Object.keys(dependencies).filter(isDropped).forEach(dependency => {
-  test(`${dependency} loads ok`, t => {
-    const module = require(dependency)
-    t.truthy(module)
-  })
-})
+Object.keys(dependencies).filter(isDropped).forEach((dependency) => {
+  test(`${dependency} loads ok`, (t) => {
+    const module = require(`${dependency}`);
+    t.truthy(module);
+  });
+});
   
-Object.keys(devDependencies).filter(isDropped).forEach(dependency => {
-  test(`${dependency} loads ok`, t => {
-    const module = require(dependency)
-    t.truthy(module)
+Object.keys(devDependencies).filter(isDropped).forEach((dependency) => {
+  test(`${dependency} loads ok`, (t) => {
+    const module = require(`${dependency}`);
+    t.truthy(module);
   })
-})
+});
