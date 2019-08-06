a65e8c76d22e57a01ee734364ee38cc476db8b9d
Manuel J. Bernal <manu.asi2@gmail.com>
2019-08-07 00:27:22 +0200

    Fixed quiz

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bot = new Bot(process.env.TOKEN, process.env.NODE, process.env.dburl)
 require('./lib/app')(process.env.TOKEN)
 
 // Loading modules...
-bot.start().then( () => {}).catch(e =>{
-  console.error('error in somewhere at starting bot ',e)
+bot.start().then( () => {
+  console.log('Started bot')
+}).catch(e =>{
+  console.error('Error starting bot. ',e.message || e)
 })
