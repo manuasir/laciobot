881ce5b273b9b789d7c7359586624b1d5b4b10ec
manuasir <manuel.jimenez@wazuh.com>
2020-11-21 01:07:41 +0100

    Adding mocks and fixing multi-chat quiz

--- src/trivia/trivia.js
+++ src/trivia/trivia.js
@@ -35,11 +35,11 @@ class Trivia {
       // Send confirm check
       this.bot.answerCallbackQuery(msg.id)
       const data = JSON.parse(msg.data)
-      const [chatId, messageId] = this.lastMessage
-      //this.lastMessage[msg.chat.id][messageId]
-      console.log(data)
+      const chatId = msg.message.chat.id
+      const messageId = this.lastMessage[chatId]
       const replyMarkup = this.updateKeyboard(data.isCorrect)
       // Edit message markup
+      delete this.lastMessage[chatId]
       return this.bot.editMessageReplyMarkup({ chatId, messageId }, { replyMarkup })
     } catch (error) {
       console.error(error.message || error)
@@ -56,9 +56,10 @@ class Trivia {
 
   async translate(words) {
     try {
-      const result = await got(`${this.translateUrl}gtx&sl=en&tl=es&dt=t&q=${words}`)
-      const jsonRes = JSON.parse(result.body)
-      return jsonRes[0][0][0]
+      // const result = await got(`${this.translateUrl}gtx&sl=en&tl=es&dt=t&q=${words}`)
+      // const jsonRes = JSON.parse(result.body)
+      // return jsonRes[0][0][0]
+      return `'Brendan Fraser protagonizó las siguientes películas, excepto ¿cuál?; Monkeybone; Encino Man; Mrs. '`
     } catch (error) {
       return Promise.reject(error.message || error)
     }
@@ -66,13 +67,11 @@ class Trivia {
 
   async translateFullQuestion(question) {
     try {
-      const msgs = []
-      msgs.push(await this.translate(question.question))
-      for (let item of question.answers) {
-        msgs.push(await this.translate(item.answer))
-      }
-      question['question'] = msgs[0]
-      question.answers.map((item, i) => item.answer = `${msgs[i + 1]} (${item.answer})`)
+      const str = `${question.question};${question.answers.map(item => item.answer).join(';')}`
+      const msgs = await this.translate(str)
+      const splittedAnswers = msgs.split(';')
+      question['question'] = splittedAnswers[0]
+      question.answers.map((item, i) => item.answer = `${splittedAnswers[i + 1]} (${item.answer})`)
       return question
     } catch (error) {
       return Promise.reject(error.message || error)
@@ -81,44 +80,57 @@ class Trivia {
 
   async makeQuestion(msg) {
     try {
+      if (this.lastMessage[msg.chat.id]) {
+        return msg.reply.text('Hay preguntas pendientes bish.')
+      }
       const category = msg.text.split(' ')[1]
       const categoryId = categories[category]
       if (!category || !categoryId || isNaN(categoryId)) {
         return this.help(msg)
       }
-      const question = await this.getFormattedQuestion(categoryId)
+      const question = await this.getFormattedQuestion(categoryId, msg.chat.id)
       const translatedQuestion = await this.translateFullQuestion(question)
-      const buttons = translatedQuestion.answers.map((item) => { return [this.bot.inlineButton(item.answer, { callback: JSON.stringify(item) })] })
+      const buttons = translatedQuestion.answers.map((item) => {
+        return [this.bot.inlineButton(item.answer, {
+          callback: JSON.stringify(item)
+        })]
+      })
       const replyMarkup = this.bot.inlineKeyboard(buttons, { resize: true })
-      const data = await this.bot.sendMessage(msg.from.id, translatedQuestion.question, { replyMarkup })
-      this.lastMessage = [msg.from.id, data.message_id]
+      const data = await this.bot.sendMessage(msg.chat.id, translatedQuestion.question, { replyMarkup })
+      this.lastMessage[msg.chat.id] = data.message_id
     } catch (error) {
       console.error(error.message || error)
       msg.reply.text('No pude generar la pregunta 😔 sorry pish')
     }
   }
 
-  async getFormattedQuestion(catId) {
+  async getFormattedQuestion(catId, chatId) {
     try {
-      const result = await got(`${this.url}&category=${catId}`)
-      console.log(result.body)
+      // const result = await got(`${this.url}&category=${catId}`)
 
-      if (!result || !result.body || typeof result.body !== 'string' || result.body.length === 0) {
-        throw new Error('Empty question')
-      }
-      const questionObj = JSON.parse(result.body)
-      if (!questionObj || !Array.isArray(questionObj.results) || questionObj.results.length === 0) {
-        throw new Error('Empty question')
-      }
-      const finalQuestion = questionObj.results[0]
-      const finalObj = {
-        question: this.entities.decode(finalQuestion.question),
+      // if (!result || !result.body || typeof result.body !== 'string' || result.body.length === 0) {
+      //   throw new Error('Empty question')
+      // }
+      // const questionObj = JSON.parse(result.body)
+      // if (!questionObj || !Array.isArray(questionObj.results) || questionObj.results.length === 0) {
+      //   throw new Error('Empty question')
+      // }
+      // const finalQuestion = questionObj.results[0]
+      // const finalObj = {
+      //   question: this.entities.decode(finalQuestion.question),
+      //   answers: [
+      //     ...finalQuestion.incorrect_answers.map((item) => { return { answer: item, isCorrect: false } }),
+      //     { answer: finalQuestion.correct_answer, isCorrect: true }
+      //   ]
+      // }
+      // return finalObj
+      return {
+        question: 'The colour of the pills in the Matrix were Blue and Yellow.',
         answers: [
-          ...finalQuestion.incorrect_answers.map((item) => { return { answer: item, isCorrect: false } }),
-          { answer: finalQuestion.correct_answer, isCorrect: true }
+          { answer: 'True', isCorrect: true },
+          { answer: 'False', isCorrect: false }
         ]
       }
-      return finalObj
     } catch (error) {
       return Promise.reject(error)
     }
